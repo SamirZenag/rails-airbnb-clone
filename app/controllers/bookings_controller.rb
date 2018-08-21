e60b10be681110cac93c2407efe565f1f8fbcf86
ajonville <ajonvill@yahoo.fr>
2018-08-21 11:47:43 +0200

    modification of the controller

--- app/controllers/bookings_controller.rb
+++ app/controllers/bookings_controller.rb
@@ -1,2 +1,48 @@
 class BookingsController < ApplicationController
+  before_action :set_booking, only: [:show, :edit, :update, :destroy]
+
+  def index
+    @bookings = Booking.all
+  end
+
+  def new
+    @booking = Booking.new
+  end
+
+  def create
+    @booking = Booking.new(booking_params)
+    if @booking.save
+      redirect_to booking_path(@booking)
+    else
+      render :new
+    end
+  end
+
+  def show
+  end
+
+  def edit
+  end
+
+  def update
+    @booking.update(booking_params)
+    # no need for app/views/bookings/create.html.erb
+    redirect_to booking_path(@booking)
+  end
+
+  def destroy
+    @booking.destroy
+    # no need for app/views/bookings/destroy.html.erb
+    redirect_to bookings_path
+  end
+
+  private
+
+  def booking_params
+    params.require(:booking).permit(:start_date, :end_date, :review, :rating)
+  end
+
+  def set_booking
+    @booking = Booking.find(params[:id])
+  end
 end
--- app/controllers/cars_controller.rb
+++ app/controllers/cars_controller.rb
@@ -1,2 +1,48 @@
 class CarsController < ApplicationController
+  before_action :set_car, only: [:show, :edit, :update, :destroy]
+
+  def index
+    @cars = Car.all
+  end
+
+  def new
+    @car = Car.new
+  end
+
+  def create
+    @car = Car.new(car_params)
+    if @car.save
+      redirect_to car_path(@car)
+    else
+      render :new
+    end
+  end
+
+  def show
+  end
+
+  def edit
+  end
+
+  def update
+    @car.update(car_params)
+    # no need for app/views/cars/create.html.erb
+    redirect_to car_path(@car)
+  end
+
+  def destroy
+    @car.destroy
+    # no need for app/views/cars/destroy.html.erb
+    redirect_to cars_path
+  end
+
+  private
+
+  def car_params
+    params.require(:car).permit(:brand, :model, :description, :price, :lat, :lng, :address, :city, :postal_code, :country)
+  end
+
+  def set_car
+    @car = Car.find(params[:id])
+  end
 end
