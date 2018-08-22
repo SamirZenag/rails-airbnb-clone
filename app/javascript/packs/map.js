ac8acbc538bfbb6435b2284d6529448340434c44
ajonville <ajonvill@yahoo.fr>
2018-08-22 16:45:05 +0200

    Index ok

--- app/javascript/packs/map.js
+++ app/javascript/packs/map.js
@@ -1,23 +1,6 @@
 import GMaps from 'gmaps/gmaps.js';
 import { autocomplete } from '../components/autocomplete';
 
-const mapElement = document.getElementById('map');
-if (mapElement) { // don't try to build a map if there's no div#map to inject in
-  const map = new GMaps({ el: '#map', lat: 0, lng: 0 });
-  const markers = JSON.parse(mapElement.dataset.markers);
-  map.addMarkers(markers);
-  if (markers.length === 0) {
-    map.setZoom(2);
-  } else if (markers.length === 1) {
-    map.setCenter(markers[0].lat, markers[0].lng);
-    map.setZoom(14);
-  } else {
-    map.fitLatLngBounds(markers);
-  }
-}
-
-autocomplete();
-
 const styles = [
     {
         "featureType": "landscape",
@@ -121,9 +104,37 @@ const styles = [
             }
         ]
     }
+]
 
-map.addStyle({
-  styles: styles,
-  mapTypeId: 'map_style'
-});
-map.setStyle('map_style');
+const mapElement = document.getElementById('map');
+if (mapElement) { // don't try to build a map if there's no div#map to inject in
+  const map = new GMaps({ el: '#map', lat: 0, lng: 0 });
+  map.addStyle({
+    styles: styles,
+    mapTypeId: 'map_style'
+  });
+  map.setStyle('map_style');
+  const markers = JSON.parse(mapElement.dataset.markers);
+  map.addMarkers(markers);
+  if (markers.length === 0) {
+    map.setZoom(2);
+  } else if (markers.length === 1) {
+    map.setCenter(markers[0].lat, markers[0].lng);
+    map.setZoom(14);
+  } else {
+    map.fitLatLngBounds(markers);
+  }
+
+  const button = document.getElementById('map-button')
+  const list = document.getElementById('list')
+  mapElement.classList.add('hidden')
+  button.addEventListener('click', (event) => {
+    mapElement.classList.toggle('hidden')
+    list.classList.toggle('hidden')
+  })
+
+}
+
+autocomplete();
+
+
